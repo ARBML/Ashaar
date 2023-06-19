0dbbb7be770bd69a6f3efd979d81201364f221fa
zaidalyafeai <alyafey22@gmail.com>
2023-06-19 14:19:47 +0300

    make single demo for arabic and english

--- app.py
+++ app.py
@@ -5,29 +5,33 @@ from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from Ashaar.utils import get_output_df, display_highlighted_patterns, get_highlighted_patterns_html
 from Ashaar.bait_analysis import BaitAnalysis
+from langs import *
 import sys
 import json
+import argparse
 
-TITLE="""<h1 style="font-size: 30px;" align="center">Ashaar: Arabic Poetry Analysis and Generation</h1>"""
-DESCRIPTION = """
-<p align = 'center'>
-<img src='https://raw.githubusercontent.com/ARBML/Ashaar/master/images/ashaar_icon.png' width='150px' alt='logo for Ashaar'/>
-</p>
-"""
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('--lang', type = str, default = 'ar', required=True)
+args = arg_parser.parse_args()
+lang = args.lang
 
-DESCRIPTION +="""
-The demo provides a way to generate analysis for poetry and also complete the poetry. 
-The generative model is a character-based conditional GPT-2 model. The pipeline contains many models for 
-classification, diacritization and conditional generation. Check our [GitHub](https://github.com/ARBML/Ashaar) for more techincal details
-about this work. In the demo we have two basic pipelines. `Analyze` which predicts the meter, era, theme, diacritized text, qafiyah and, arudi style.
-The other module `Generate` which takes the input text, meter, theme and qafiyah to generate the full poem. 
-"""
-if (SPACE_ID := os.getenv('SPACE_ID')) is not None:
-    DESCRIPTION = DESCRIPTION.replace("</p>", " ")
-    DESCRIPTION += f'or <a href="https://huggingface.co/spaces/{SPACE_ID}?duplicate=true"><img style="display: inline; margin-top: 0em; margin-bottom: 0em" src="https://bit.ly/3gLdBN6" alt="Duplicate the Space"/></a> and upgrade to GPU in settings.</p>'
+if lang == 'ar':
+    TITLE = TITLE_ar
+    DESCRIPTION = DESCRIPTION_ar
+    textbox_trg_text = textbox_trg_text_ar
+    textbox_inp_text = textbox_inp_text_ar
+    btn_trg_text = btn_trg_text_ar
+    btn_inp_text = btn_inp_text_ar
+    css = """ #textbox{ direction: RTL;}"""
+
 else:
-    DESCRIPTION = DESCRIPTION.replace("either", "")
-
+    TITLE = TITLE_en
+    DESCRIPTION = DESCRIPTION_en
+    textbox_trg_text = textbox_trg_text_en
+    textbox_inp_text = textbox_inp_text_en
+    btn_trg_text = btn_trg_text_en
+    btn_inp_text = btn_inp_text_en
+    css = ""
 
 gpt_tokenizer = AutoTokenizer.from_pretrained('arbml/ashaar_tokenizer')
 model = AutoModelForCausalLM.from_pretrained('arbml/Ashaar_model')
@@ -93,37 +97,43 @@ def generate(inputs, top_p = 3):
 examples = [
     [
 """القلب أعلم يا عذول بدائه
-وأحق منك بجفنه وبمائه
-مهلا فإن العذل من أسقامه
-وترفقا فالسمع من أعضائه"""
+وأحق منك بجفنه وبمائه"""
+    ],
+    [
+"""ألا ليت شعري هل أبيتن ليلة
+بجنب الغضى أزجي الغلاص النواجيا"""
     ],
 ]
 
-with gr.Blocks(theme=gr.themes.Soft()) as demo:
+with gr.Blocks(theme=gr.themes.Soft(), css=css) as demo:
     with gr.Row():
         with gr.Column():
             gr.HTML(TITLE)
-            gr.Markdown(DESCRIPTION)
+            gr.HTML(DESCRIPTION)
 
     with gr.Row():
         with gr.Column():
-            gr.Markdown("Write the input, one verse in each line")
-            inputs = gr.inputs.Textbox(lines=10, label="Input Poem")
+            textbox_output = gr.Textbox(lines=10, label=textbox_trg_text, elem_id="textbox")
         with gr.Column():
-            gr.Markdown("Output generated poem")
-            outputs_2 = gr.inputs.Textbox(lines=10, label="Generated Poem")
+            inputs = gr.Textbox(lines=10, label=textbox_inp_text, elem_id="textbox")
+
 
     with gr.Row():
         with gr.Column():
-            analyze_btn = gr.Button("Analyze")
+            trg_btn = gr.Button(btn_trg_text)
         with gr.Column():
-            generate_btn = gr.Button("Generate")
+            inp_btn = gr.Button(btn_inp_text)
 
     with gr.Row():
-        outputs_1 = gr.HTML()
-    
-    gr.Examples(examples, inputs)
-    analyze_btn.click(analyze, inputs=inputs, outputs=outputs_1)
-    generate_btn.click(generate, inputs = inputs, outputs=outputs_2)
+        html_output = gr.HTML()
+        
+    if lang == 'en':
+        gr.Examples(examples, textbox_output)
+        inp_btn.click(generate, inputs = textbox_output, outputs=inputs)
+        trg_btn.click(analyze, inputs = textbox_output, outputs=html_output)
+    else:
+        gr.Examples(examples, inputs)
+        trg_btn.click(generate, inputs = inputs, outputs=textbox_output)
+        inp_btn.click(analyze, inputs = inputs, outputs=html_output)
 
 demo.launch(server_name = "0.0.0.0", share = True, debug = True)
---
+++ langs.py
@@ -0,0 +1,43 @@
+IMG = """<p align = 'center'>
+<img src='https://raw.githubusercontent.com/ARBML/Ashaar/master/images/ashaar_icon.png' width='150px' alt='logo for Ashaar'/>
+</p>
+
+"""
+TITLE_ar="""<h1 style="font-size: 30px;" align="center">أَشْعــَـار: تحليل وإنشاء الشعر العربي</h1>"""
+DESCRIPTION_ar = IMG
+
+DESCRIPTION_ar +=""" <p dir='rtl'>
+هذا البرنامج يتيح للمستخدم تحليل وإنشاء الشعر العربي.
+يتم إنشاء الشعر بواسطة نموذج GPT تم تدريبه على الحروف العربية. 
+يحتوي البرنامج على نماذج لتصنيف الحقبة الزمنية والعاطفة والبحر و كذلك تشكيل الشعر العربي بالإضافة إلى إكمال الشعر.
+قمنا بتوفير الشفرة البرمجية كلها على 
+<a href ='https://github.com/ARBML/Ashaar'> GitHub</a>.
+
+</p>
+"""
+
+TITLE_en="""<h1 style="font-size: 30px;" align="center">Ashaar: Arabic Poetry Analysis and Generation</h1>"""
+DESCRIPTION_en = IMG
+
+DESCRIPTION_en +="""
+The demo provides a way to generate analysis for poetry and also complete the poetry. 
+The generative model is a character-based conditional GPT-2 model. The pipeline contains many models for 
+classification, diacritization and conditional generation. Check our <a src='https://github.com/ARBML/Ashaar'>GitHub</a> for more techincal details
+about this work. In the demo we have two basic pipelines. Analyze which predicts the meter, era, theme, diacritized text, qafiyah and, arudi style.
+The other module, Generate which takes the input text, meter, theme and qafiyah to generate the full poem. 
+"""
+
+btn_trg_text_ar = "إنشاء"
+btn_inp_text_ar = "تحليل"
+
+btn_inp_text_en = "Generate"
+btn_trg_text_en = "Analyze"
+
+textbox_inp_text_ar = "القصيدة المدخلة"
+textbox_trg_text_ar = "القصيدة المنشئة"
+
+textbox_trg_text_en = "Input Poem"
+textbox_inp_text_en = "Generated Poem"
+
+
+
